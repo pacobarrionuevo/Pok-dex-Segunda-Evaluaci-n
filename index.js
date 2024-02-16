4c1d6eed357b36621759bc6a9826dd03102c1096
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-02-16 17:28:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,96 @@
-getPokemonData(13);
-getEvolutionChain(13);
-async function getPokemonData(id) {
-    const obj = await getData(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    console.log(obj);
-    document.getElementById('imagen').src = obj.sprites.other["showdown"].front_default;
-    document.getElementById('name').innerText = obj.name;
-    document.getElementById('ability').innerText = obj.abilities[0].ability.name;
+const arrayPokemonTodos = [];
+
+pokemon();
+
+async function pokemon() {
+    await obtenerPokemons();
+    drawPokemons(arrayPokemonTodos);
+} 
+
+async function obtenerPokemons() {
+    for (let i = 1; i <= 151; i++) {
+        const pokemon = await getData(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        arrayPokemonTodos.push(pokemon);
+    }
+}
+
+function drawPokemons(pokemons) {
+    const listaPokemon = document.getElementById('lista-pokemon');
+    for(const pokemon of pokemons){
+        const pokemonCard = document.createElement('div');
+        //HACER UNA FUNCIÓN DONDE GUARDAR ESTO
+        //Objetivo: hacer que si un pokemon tiene doble tipo, una mitad sea de un color y la otra de otro.
+        switch (pokemon.types[0].type.name) {
+            case 'grass':
+                pokemonCard.classList.add('pokemonCardPlanta');
+                break;
+            case 'bug':
+                pokemonCard.classList.add('pokemonCardBicho');
+                break;
+            case 'fire':
+                pokemonCard.classList.add('pokemonCardFuego');
+                break;
+            case 'water':
+                pokemonCard.classList.add('pokemonCardAgua');
+                break;
+            case 'ice':
+                pokemonCard.classList.add('pokemonCardHielo');
+                break;
+            case 'electric':
+                pokemonCard.classList.add('pokemonCardElectrico');
+                break;
+            case 'ground':
+                pokemonCard.classList.add('pokemonCardTierra');
+            break;
+            case 'rock':
+                pokemonCard.classList.add('pokemonCardRoca');
+            break;
+            case 'fighting':
+                pokemonCard.classList.add('pokemonCardLucha');
+                break;
+            case 'steel':
+                pokemonCard.classList.add('pokemonCardAcero');
+                break;
+            case 'psychic':
+                pokemonCard.classList.add('pokemonCardPsiquico');
+                break;
+            case 'poison':
+                pokemonCard.classList.add('pokemonCardVeneno');
+                break;
+            case 'fairy':
+                pokemonCard.classList.add('pokemonCardHada');
+                break;
+            case 'normal':
+                pokemonCard.classList.add('pokemonCardNormal');
+                break;
+            case 'dragon':
+                pokemonCard.classList.add('pokemonCardDragon');
+                break;
+            case 'ghost':
+                pokemonCard.classList.add('pokemonCardFantasma');
+                break;
+            default:
+                pokemonCard.classList.add('pokemonCard');
+                break;
+        }
+        //listaPokemon.innerHTML += '<div class="pokemon-card">';
+        pokemonCard.innerHTML += '<div style = "text-align: center;">' + pokemon.name + ' ( '+pokemon.id+' )' + '</div>';
+        pokemonCard.innerHTML += '<img src=' + pokemon.sprites.other["official-artwork"].front_default + ' style="height=200px; display: grid; margin: 0 auto;">';
+        if (pokemon.types.length>1) {
+            pokemonCard.innerHTML += '<div style = "text-align: center;">' + pokemon.types[0].type.name + '/' + pokemon.types[1].type.name +'</div>';
+        } else {
+            pokemonCard.innerHTML += '<div style = "text-align: center;">' + pokemon.types[0].type.name + '</div>'
+        }
+        
+        listaPokemon.appendChild(pokemonCard);
+        //listaPokemon.innerHTML += '</div>'
+        
+    }
     
 }
+
 async function getData(url) {
-    const response = await fetch (url);
+    const response = await fetch(url);
     const json = await response.text();
     return JSON.parse(json);
 }
-
-async function getEvolutionChain(id) {
-    const obj = await getData(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
-    const evolutionChainURL = obj.evolution_chain.url;
-    const evolutionChain = await getData(evolutionChainURL)
-    console.log(evolutionChain);
-}
-
