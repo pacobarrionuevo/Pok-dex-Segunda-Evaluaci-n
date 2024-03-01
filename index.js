a39d70199a3ab36abf06a71779c2101416e7dcc7
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-03-01 20:04:20 +0100

    Update index.js SIIIIIIIIIIIII

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { traducirTipo as _traducirTipo } from "./traducirTipo.js";
+
+const buscador = document.getElementById('barraBusqueda'); 
+const listaPokemon = document.getElementById('lista-pokemon');
 const arrayPokemonTodos = [];
 
 pokemon();
@@ -9,22 +13,21 @@ async function pokemon() {
 
 async function obtenerPokemons() {
     for (let i = 1; i <= 151; i++) {
-        const pokemon = await getData(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        let pokemon = await getData(`https://pokeapi.co/api/v2/pokemon/${i}`);
         arrayPokemonTodos.push(pokemon);
     }
 }
 
 function drawPokemons(pokemons) {
-    const listaPokemon = document.getElementById('lista-pokemon');
+    listaPokemon.innerHTML = '';
     for(const pokemon of pokemons){
         const pokemonCard = document.createElement('div');
         pokemonCard.classList.add('pokemonCard');
         pokemonCard.innerHTML += '<a href="vistainterna.html">'+'<div style = "text-align: center;">' + pokemon.name + ' ('+pokemon.id+')' + '</div>';
-
         pokemonCard.innerHTML += '<img src=' + pokemon.sprites.other["official-artwork"].front_default + ' style="height=200px; display: grid; margin: 0 auto;">';
-        pokemonCard.innerHTML += '<div style = "text-align: center;" class="'+ cambiarColor(pokemon.types[0].type.name) +'">' + traducirTipo(pokemon.types[0].type.name) +'</div>';
+        pokemonCard.innerHTML += '<div style = "text-align: center;" class="'+ cambiarColor(pokemon.types[0].type.name) +'">' + _traducirTipo(pokemon.types[0].type.name) +'</div>';
         if (pokemon.types.length > 1) {
-            pokemonCard.innerHTML += '<div style = "text-align: center;" class="'+ cambiarColor(pokemon.types[1].type.name) +'">' + traducirTipo(pokemon.types[1].type.name) +'</div>';
+            pokemonCard.innerHTML += '<div style = "text-align: center;" class="'+ cambiarColor(pokemon.types[1].type.name) +'">' + _traducirTipo(pokemon.types[1].type.name) +'</div>';
         }
         pokemonCard.innerHTML += '</a>';
         listaPokemon.appendChild(pokemonCard);
@@ -33,67 +36,8 @@ function drawPokemons(pokemons) {
 
 async function getData(url) {
     const response = await fetch(url);
-    const json = await response.text();
-    return JSON.parse(json);
-}
-
-function traducirTipo(pokemonTipo) {
-    switch (pokemonTipo) {
-        case 'grass':
-            return 'planta';
-            break;
-        case 'bug':
-            return 'bicho';
-            break;
-        case 'fire':
-            return 'fuego';
-            break;
-        case 'water':
-            return 'agua';
-            break;
-        case 'ice':
-            return 'hielo';
-            break;
-        case 'electric':
-            return 'eléctrico';
-            break;
-        case 'ground':
-            return 'tierra';
-        break;
-        case 'rock':
-            return 'roca';
-        break;
-        case 'fighting':
-            return 'lucha';
-            break;
-        case 'steel':
-            return 'acero';
-            break;
-        case 'psychic':
-            return 'psíquico';
-            break;
-        case 'poison':
-            return 'veneno';
-            break;
-        case 'fairy':
-            return 'hada';
-            break;
-        case 'normal':
-            return 'normal';
-            break;
-        case 'dragon':
-            return 'dragón';
-            break;
-        case 'ghost':
-            return 'fantasma';
-            break;
-        case 'flying':
-            return 'volador';
-            break;
-        default:
-            return 'no tiene tipo';
-            break;
-    }
+    const json = await response.json();
+    return json;
 }
 
 function cambiarColor(tipo) {
@@ -118,10 +62,10 @@ function cambiarColor(tipo) {
             break;
         case 'ground':
             return 'pokemonCardTierra';
-        break;
+            break;
         case 'rock':
             return 'pokemonCardRoca';
-        break;
+            break;
         case 'fighting':
             return 'pokemonCardLucha';
             break;
@@ -154,10 +98,9 @@ function cambiarColor(tipo) {
     }
 }
 
-const botonModoClaroOscuro = document.getElementById("modo-claro-oscuro");
-
-botonModoClaroOscuro.addEventListener("click", () => {document.body.classList.toggle("modo-oscuro");});
-
-const botonModoClaroOscuro = document.getElementById("modo-claro-oscuro");
-
-botonModoClaroOscuro.addEventListener("click", () => {document.body.classList.toggle("modo-oscuro");});
+buscador.addEventListener('input', () => {
+    const searchText = buscador.value.toLowerCase().trim();
+    const pokemonEncontrados = arrayPokemonTodos.filter(pokemon => pokemon.name.includes(searchText) || pokemon.id.toString().includes(searchText));
+    console.log('Pokémon filtrados:', pokemonEncontrados);
+    drawPokemons(pokemonEncontrados);
+});
